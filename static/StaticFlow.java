035c43dc2a537604b7f9ccee1f6180a8fc886fac
ravindra2017bangalore <parihar.ravindrasingh@gmail.com>
2017-08-18 22:23:33 +0530

    first commit

---
+++ static/StaticFlow.java
@@ -0,0 +1,19 @@
+class StaticFlow 
+{
+	
+	static
+	{
+		//m1();
+		System.out.println(i);
+		m1();
+	}
+	public static void m1()
+	{
+		System.out.println(i);
+	}
+	public static void main(String[] args)
+	{
+		System.out.println("Main");
+	}
+	static int i =10;
+}
